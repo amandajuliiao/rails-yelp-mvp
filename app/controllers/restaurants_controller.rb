3072255eec17e195cbca11bb1e6442ceab789c07
Amanda Juliao <amanda.juliao@hotmail.com>
2020-04-23 09:50:51 -0300

    worked on yelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,7 @@ class RestaurantsController < ApplicationController
   before_action :find_restaurant, only: [:show]
 
   def index
-    @restaurants = Restaurants.all
+    @restaurants = Restaurant.all
   end
 
   def show; end
